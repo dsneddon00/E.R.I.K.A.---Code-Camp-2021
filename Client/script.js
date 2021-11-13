b1c84ec5599fde89d9fa42f32470b034eb29a650
Derek Sneddon <dsneddon00@gmail.com>
2021-11-13 01:36:30 -0700

    whateber

--- Client/script.js
+++ Client/script.js
@@ -1,72 +1,68 @@
-
-
-var signInButton = document.querySelector("#btn-sign-in");
-var registerButton = document.querySelector("#btn-register");
+// var signInButton = document.querySelector("#btn-sign-in");
+// var registerButton = document.querySelector("#btn-register");
 var sendChat = document.querySelector("#button-addon2")
 
 
-signInButton.onclick=function() {
+// signInButton.onclick=function() {
 
-  var email = document.querySelector("#floatingInput").value;
-  var password = document.querySelector("#floatingPassword").value;
-  var body = "userName=" + encodeURIComponent(email);
-  body += "&userPassword=" + encodeURIComponent(password);
+//   var email = document.querySelector("#floatingInput").value;
+//   var password = document.querySelector("#floatingPassword").value;
+//   var body = "userName=" + encodeURIComponent(email);
+//   body += "&userPassword=" + encodeURIComponent(password);
 
-  fetch("http://localhost:8080/sessions", {
-    method: "POST",
-    credentials: "include",
-    body: body,
-    headers: {
-      "Content-Type":"application/x-www-form-urlencoded"
-    }
-  }).then(function(response) {
-    console.log("Server responded to POST request")
+//   fetch("http://localhost:8080/sessions", {
+//     method: "POST",
+//     credentials: "include",
+//     body: body,
+//     headers: {
+//       "Content-Type":"application/x-www-form-urlencoded"
+//     }
+//   }).then(function(response) {
+//     console.log("Server responded to POST request")
 
-    if(response.status == 200) {
-      alert('Sucessfully signed in: ' + email);
-    } else if (response.status == 401) {
-      alert('Failed to sign in')
-    } else {
-      alert("Try again " + String(response.status))
-    }
-  });
+//     if(response.status == 200) {
+//       alert('Sucessfully signed in: ' + email);
+//     } else if (response.status == 401) {
+//       alert('Failed to sign in')
+//     } else {
+//       alert("Try again " + String(response.status))
+//     }
+//   });
 
-}
+// }
 
-registerButton.onclick=function() {
+// registerButton.onclick = function() {
+//   var email = document.querySelector("#floatingInput").value;
+//   var password = document.querySelector("#floatingPassword").value;
+//   var data = "userName=" + encodeURIComponent(email);
+//   data += "&userPassword=" + encodeURIComponent(password);
 
-  var email = document.querySelector("#floatingInput").value;
-  var password = document.querySelector("#floatingPassword").value;
-  var data = "userName=" + encodeURIComponent(email);
-  data += "&userPassword=" + encodeURIComponent(password);
+//   fetch("http://localhost:8080/users", {
+//     method: "POST",
+//     credentials: "include",
+//     body: data,
+//     headers: {
+//       "Content-Type":"application/x-www-form-urlencoded"
+//     }
+//   }).then(function(response) {
+//     console.log("Server responded to POST request")
 
-  fetch("http://localhost:8080/users", {
-    method: "POST",
-    credentials: "include",
-    body: data,
-    headers: {
-      "Content-Type":"application/x-www-form-urlencoded"
-    }
-  }).then(function(response) {
-    console.log("Server responded to POST request")
+//     if(response.status == 201) {
+//       alert('Sucessful Registration');
+//     } else if (response.status == 401) {
+//       alert('Email Taken')
+//     } else {
+//       alert("Try again " + String(response.status))
+//     }
+//   });
+// }
 
-    if(response.status == 201) {
-      alert('Sucessful Registration');
-    } else if (response.status == 401) {
-      alert('Email Taken')
-    } else {
-      alert("Try again " + String(response.status))
-    }
-  });
-
-  
-}
-sendChat.onclick=function() {
-  var bot =document.getElementById("#bot")
-  var you = document.getElementById("#you")
-  fetch("https://localhost:8080/chat?userID=test65&userInput="+ str(you.value))
+sendChat.onclick = function() {
+  var you = document.querySelector("#you").value
+  var bot = document.querySelector("#bot")
+  fetch("https://localhost:8080/chat?userID=test65&userInput="+ you)
     .then(response => response.json())
     .then(data => console.log(data))   
   bot.value = data
-
+  console.log(data)
 }
