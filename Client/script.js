bb5d4ebcd03523a7b384b5642106df088bad423a
Derek Sneddon <dsneddon00@gmail.com>
2021-11-13 03:12:50 -0700

    index page

--- Client/script.js
+++ Client/script.js
@@ -2,7 +2,6 @@
 // var registerButton = document.querySelector("#btn-register");
 var sendChat = document.querySelector("#button-addon2")
 
-
 // signInButton.onclick=function() {
 
 //   var email = document.querySelector("#floatingInput").value;
